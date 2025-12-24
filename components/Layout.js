4c609d638a7177ff4812f05eeba34cbe81653551
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 12:17:27 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,32 @@
-import Nav from "./Nav";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Nav />
-      <main style={{ padding: "24px" }}>
+      <nav
+        style={{
+          background: "#4b2e83",
+          padding: "10px 16px"
+        }}
+      >
+        <div
+          style={{
+            maxWidth: 1100,
+            margin: "0 auto",
+            display: "flex",
+            gap: 16
+          }}
+        >
+          <Link href="/" style={{ color: "white" }}>Home</Link>
+          <Link href="/programme" style={{ color: "white" }}>Programme</Link>
+          <Link href="/students" style={{ color: "white" }}>Students</Link>
+          <Link href="/tasks" style={{ color: "white" }}>Tasks</Link>
+          <Link href="/exams" style={{ color: "white" }}>Exams</Link>
+          <Link href="/accreditation" style={{ color: "white" }}>Accreditation</Link>
+        </div>
+      </nav>
+
+      <main className="container">
         {children}
       </main>
     </>
