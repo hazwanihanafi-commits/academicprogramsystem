1a304debd0ea34705946b4b56625d0655ddad40d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 09:33:17 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,12 @@
-export default ({children})=>children;
+import Nav from "./Nav";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Nav />
+      <main style={{ padding: "24px" }}>
+        {children}
+      </main>
+    </>
+  );
+}
