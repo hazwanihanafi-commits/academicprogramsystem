91e77f84cb84f5d571b0ee81537d385a249e4e25
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 09:32:55 +0800

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,1 +1,15 @@
-export default ()=>null;
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav style={{ background: "#4b2e83", padding: "12px" }}>
+      <Link href="/" style={{ color: "white", marginRight: 15 }}>Dashboard</Link>
+      <Link href="/programme" style={{ color: "white", marginRight: 15 }}>Programme</Link>
+      <Link href="/students" style={{ color: "white", marginRight: 15 }}>Students</Link>
+      <Link href="/cqi" style={{ color: "white", marginRight: 15 }}>CQI</Link>
+      <Link href="/accreditation" style={{ color: "white", marginRight: 15 }}>Accreditation</Link>
+      <Link href="/exams" style={{ color: "white", marginRight: 15 }}>Exams</Link>
+      <Link href="/tasks" style={{ color: "white" }}>Tasks</Link>
+    </nav>
+  );
+}
