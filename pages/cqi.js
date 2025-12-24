c60d78dbe860de40f64a59cddf47576e921b5216
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 09:37:40 +0800

    Create cqi.js

---
+++ pages/cqi.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+
+export default function CQI() {
+  return (
+    <Layout>
+      <h1>Continuous Quality Improvement (CQI)</h1>
+
+      <div style={{ background: "white", padding: 20, borderRadius: 6 }}>
+        <p><b>Identified Issue:</b> Practical competency below benchmark</p>
+        <p><b>Action Taken:</b> Increased supervised clinical sessions</p>
+        <p><b>Status:</b> Monitoring in progress</p>
+        <p><b>Review Cycle:</b> Next semester</p>
+      </div>
+    </Layout>
+  );
+}
