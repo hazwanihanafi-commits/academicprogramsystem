fd8e798e1f94724ca3ddd768f6e81a460a091de4
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 09:33:39 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-export default ()=> <h1>Program Head Companion</h1>;
+import Layout from "../components/Layout";
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <h1>Program Head Companion</h1>
+      <p>Welcome to the Program Head management system.</p>
+
+      <ul>
+        <li>📘 Programme governance</li>
+        <li>🎓 Student management</li>
+        <li>🧪 Ikhtisas examination tracking</li>
+        <li>🔄 CQI monitoring</li>
+        <li>🏛 Accreditation readiness</li>
+      </ul>
+    </Layout>
+  );
+}
